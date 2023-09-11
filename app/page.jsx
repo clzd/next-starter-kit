2874926cc8ecbeb8a561bcf365516cfae96ac83c
Your Name <anthony.@example.com>
2023-09-10 20:04:47 -0400

    intial commit

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className="container">
-        <h1 className='text-3xl font-bold mt-3'>NextJS</h1>
+        <h1 className='mt-4 text-3xl font-bold '>NextJS</h1>
       </div>
     </section>
   )
