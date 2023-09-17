fe03e51820aa9f4a1104862ebf4d250452a86f29
Your Name <anthony.calzadilla@gmail.com>
2023-09-16 23:00:37 -0400

    added typography plugin 18:39 in

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -8,8 +8,8 @@ const Page = async ({ params }) => {
 		<section className="py-24">
 			<div className="container">
 				<header className="rounded bg-gray-100 p-8">
-					<h1 className="font-serif text-3x1">Learn NextJS</h1>
-					<p className="text-sm font-light uppercase">CLZD</p>
+					<h1 className="font-serif text-3x1">{frontmatter.title}</h1>
+					<p className="text-sm font-light uppercase">{frontmatter.author}</p>
 				</header>
 
 				{/* post content */}
