5be8f26df389d899cbfebb774806f4a4754465df
Your Name <anthony.@example.com>
2023-09-17 15:29:49 -0400

    3.3 - 24:49 in

---
+++ app/components/Newsletter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Newsletter = () => {
+	return (
+		<section className="not-prose">
+			<div className="rounded bg-sky-500 p-6 text-white">
+				<p className="text-base font-medium">Subscribe to my newsletter.</p>
+			</div>
+		</section>
+
+
+	)
+}
+
+export default Newsletter
